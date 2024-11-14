89fc95cf9e005b692f499965ff1f0c2735777e14
Tijn <99891906+tijnndev@users.noreply.github.com>
2024-11-14 11:16:20 +0100

    taskbar updates

--- public/DesktopApp.jsx
+++ public/DesktopApp.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 
-function DesktopApp({ app_name, in_docker }) {
+function DesktopApp({ app_name, in_docker, isActive }) {
     const [appData, setAppData] = useState('')
     useEffect(() => {
         setAppData(app_name)
     })
 
+    let activeStyle = {
+        backgroundColor: isActive ? 'green' : 'gray',
+    }
+
     return (
-        <div className="appPadding flex flexColumn gap1rem">
+        <div style={activeStyle} className={`app-icon appPadding flex flexColumn gap1rem`}>
             <img src={`/public/app_img_${appData}.jpg`} alt={` /public/app_img_${appData}.jpg`} />
             {!in_docker &&
             <p>{appData}</p>}
--- public/Taskbar.jsx
+++ public/Taskbar.jsx
@@ -1,17 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import DesktopApp from './DesktopApp';
 
 function Taskbar() {
+  const [time, setTime] = useState(new Date());
 
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 
   return (
     <div className="taskbar">
       <div className="taskbar-start">
-        <button className="start-button">Start</button>
+        <DesktopApp 
+            app_name='windowsce'
+            in_docker={true}
+            isActive={true}
+            />
+        <DesktopApp 
+            app_name='cmd'
+            in_docker={true}
+            isActive={false}
+            />
       </div>
       <div className="taskbar-apps">
-        <div className="app-icon">App 1</div>
-        <div className="app-icon">App 2</div>
       </div>
+      <div className="taskbar-clock">{formattedTime}</div>
     </div>
   );
 }
