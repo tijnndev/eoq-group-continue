ca7cda1dead3aa87ebd37d4f0ad8ae0327794dea
lajos <lszabo958@gmail.com>
2024-11-14 11:14:24 +0100

    made desktopApp component

--- public/Desktop.jsx
+++ public/Desktop.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import App from '../src/App'
 import DesktopApp from './DesktopApp'
+import { useState, useEffect } from 'react'
 function Desktop() {
+    const [backgroundPicture, setBackgroundPicture] = useState('default_bg')
+    useEffect(() => {
+        setBackgroundPicture('default_bg')
+    })
+
   return (
     <div className='Desktop'>
+        <img src={`/public/${backgroundPicture}.jpg`} alt="" />
         <DesktopApp 
             app_name='firefox'
+            in_docker={false}
             />
     </div>
   )
